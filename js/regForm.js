dbc188dc1312130544b096fa2f01e5a06f1a4738
Rod-S <33836692+Rod-S@users.noreply.github.com>
2018-02-09 12:38:06 -0600

    "T-Shirt Info" section complete

--- js/regForm.js
+++ js/regForm.js
@@ -1,15 +1,42 @@
 //focus Basic Info - Name: on page load
 document.getElementById('name').focus();
 
-//hide job role text input until conditions met
+//initial page load to hide job role text input until conditions met
 $('#other-title').hide();
 
-//
+//conditional on select option change to show or hide job role text input
 $('#title').change(function() {
-  console.log($('#title option:selected').val());
   if ($('#title option:selected').val()=='other') {
     $('#other-title').show();
   } else {
     $('#other-title').hide();
+  }
+});
+
+//initial page laod to hide t-shirt color until conditions met
+$('#colors-js-puns').hide();
+
+//
+$('#design').change(function() {
+  if ($('#design').val()=='js puns') {
+    $('#colors-js-puns').show();
+    $('#color').val('');
+    $('#color').children('option[value="cornflowerblue"]').show();
+    $('#color').children('option[value="darkslategrey"]').show();
+    $('#color').children('option[value="gold"]').show();
+    $('#color').children('option[value="tomato"]').hide();
+    $('#color').children('option[value="steelblue"]').hide();
+    $('#color').children('option[value="dimgrey"]').hide();
+  } else if ($('#design').val()=='heart js') {
+    $('#colors-js-puns').show();
+    $('#color').val('');
+    $('#color').children('option[value="cornflowerblue"]').hide();
+    $('#color').children('option[value="darkslategrey"]').hide();
+    $('#color').children('option[value="gold"]').hide();
+    $('#color').children('option[value="tomato"]').show();
+    $('#color').children('option[value="steelblue"]').show();
+    $('#color').children('option[value="dimgrey"]').show();
+  } else {
+    $('#colors-js-puns').hide();
   }
 });
