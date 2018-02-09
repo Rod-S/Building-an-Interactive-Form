8160c6f3edc24e1a65b13045783daa4139835093
Rod-S <33836692+Rod-S@users.noreply.github.com>
2018-02-09 10:43:00 -0600

    job-role section complete

--- js/regForm.js
+++ js/regForm.js
@@ -1,1 +1,15 @@
-console.log('script linked');
+//focus Basic Info - Name: on page load
+document.getElementById('name').focus();
+
+//hide job role text input until conditions met
+$('#other-title').hide();
+
+//
+$('#title').change(function() {
+  console.log($('#title option:selected').val());
+  if ($('#title option:selected').val()=='other') {
+    $('#other-title').show();
+  } else {
+    $('#other-title').hide();
+  }
+});
