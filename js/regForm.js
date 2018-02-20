f8bbd55ab6a0fa762f054e1dfc5a31b09e24bc0d
Rod-S <33836692+Rod-S@users.noreply.github.com>
2018-02-20 13:55:24 -0600

    Completed Register for Activities - Running Total functionality.

--- js/regForm.js
+++ js/regForm.js
@@ -16,7 +16,8 @@ $('#title').change(function() {
 //initial page laod to hide t-shirt color until conditions met
 $('#colors-js-puns').hide();
 
-//
+//T-SHIRT INFO
+
 $('#design').change(function() {
   if ($('#design').val()=='js puns') {
     $('#colors-js-puns').show();
@@ -33,28 +34,70 @@ $('#design').change(function() {
   }
 });
 
+//REGISTER FOR ACTIVITIES
 
 $('input:checkbox').on('click', function () {
   let $this = $(this);
-  let isChecked = $(this).is(':checked');
+  let isChecked = $this.is(':checked');
   if ($this.is('input:checkbox[name="js-frameworks"]')) {
     if ($('input:checkbox[name="js-frameworks"]:checked')) {
       $('input:checkbox[name="express"]').prop('disabled', isChecked ? true : false);
     }
   }
-  if ($this.is('input:checkbox[name="express"]')) {
+  else if ($this.is('input:checkbox[name="express"]')) {
     if ($('input:checkbox[name="express"]:checked')) {
       $('input:checkbox[name="js-frameworks"]').prop('disabled', isChecked ? true : false);
     }
   }
-  if ($this.is('input:checkbox[name="js-libs"]')) {
+  else if ($this.is('input:checkbox[name="js-libs"]')) {
     if ($('input:checkbox[name="js-libs"]:checked')) {
       $('input:checkbox[name="node"]').prop('disabled', isChecked ? true : false);
     }
   }
-  if ($this.is('input:checkbox[name="node"]')) {
+  else if ($this.is('input:checkbox[name="node"]')) {
     if ($('input:checkbox[name="node"]:checked')) {
       $('input:checkbox[name="js-libs"]').prop('disabled', isChecked ? true : false);
     }
   }
 });
+
+//run tallyCost() on checkbox clicks
+$("input[type=checkbox]").change(function() {
+  runningTotal();
+});
+
+//tally up the cost of checked activities
+const runningTotal = () => {
+    //start cost count at $0
+    var total = 0;
+    $("input[type=checkbox]:checked").each(function() {
+      total += parseInt($(this).attr('val'));
+    });
+  $('#activityTotal').remove();
+  $('.activities').append('<div id=activityTotal>Total $' + total + '</div>');
+};
+
+//PAYMENT INFO
+
+const $creditCard = $('.credit-card');
+const $paypal = $('#credit-card').next();
+const $bitcoin = $('#credit-card').next().next();
+
+//initial payment selection
+$('#payment option[value="credit card"]').prop('selected', true);
+$paypal.hide();
+$bitcoin.hide();
+
+$('#payment').change(function() {
+  $('#payment option:selected').each(function () {
+    if ($('#payment option[value="paypal"]:selected')) {
+        $bitcoin.hide();
+        $creditCard.hide();
+        $paypal.show();
+    } else if ($('#payment option[value="credit card"]:selected')) {
+        $paypal.hide();
+        $bitcoin.hide();
+        $creditCard.show();
+    }
+  })
+})
