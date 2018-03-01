60541548e6be8ea9e641ee0ad42389f24325d0f9
Rod S <33836692+Rod-S@users.noreply.github.com>
2018-03-01 13:53:11 -0600

    final comments added

--- js/regForm.js
+++ js/regForm.js
@@ -58,6 +58,7 @@ $('input:checkbox').on('click', function () {
   let $this = $(this);
   let isChecked = $this.is(':checked');
 
+  //nested conditionals required to prevent checkbox from being permanently disabled once checked
   if ($this.is('input:checkbox[name="js-frameworks"]')) {
     if ($('input:checkbox[name="js-frameworks"]:checked')) {
       //if 'js-frameworks' is checked, disable 'express' checkbox. Otherwise, do not disable
@@ -112,7 +113,7 @@ const $creditCard = $('.credit-card');
 const $paypal = $('#credit-card').next();
 const $bitcoin = $('#credit-card').next().next();
 
-//initial payment selected option 'credit card'
+//initial payment option set to 'credit card'
 $('#payment option[value="credit card"]').prop('selected', true);
 //hide the Pay Pal and bitcoin payment messages
 $paypal.hide();
@@ -132,23 +133,15 @@ $('#payment').change(function() {
       $bitcoin.hide();
       $creditCard.hide();
       $paypal.show();
-    //if option selected is 'bitcoin'
   } else if (val === 'bitcoin') {
-      //remove classes which can potentially duplicate error messages
       $('.cc-blank-error, .cc-length-error, .cc-number-error, .zip-blank-error, .zip-length-error, .zip-number-error, .cvv-blank-error, .cvv-length-error, .cvv-number-error').hide();
       //set original bitcoin font color which can potentially be overwritten
       $('.bitMsg').css('color','#184f68');
-      //show bitcoin information
-      //hide paypal and credit card information
       $bitcoin.show();
       $creditCard.hide();
       $paypal.hide();
-    //if option selected is 'credit card'
   } else if (val === 'credit card') {
-      //remove classes which can potentially duplicate error messages
       $('.cc-blank-error, .cc-length-error, .cc-number-error, .zip-blank-error, .zip-length-error, .zip-number-error, .cvv-blank-error, .cvv-length-error, .cvv-number-error').show();
-      //hide bitcoin and paypal information
-      //show credit card information
       $bitcoin.hide();
       $creditCard.show();
       $paypal.hide();
@@ -165,17 +158,11 @@ $('#payment').change(function() {
 //FORM VALIDATION
 //////////////////
 
-/*
-//regular expression which begins with one or more words (characters[A-Za-z0-9_]) ---  ^\w+
-//then includes characters[.-] ---  \w+
-//matches [.-] either 0 or 1 time ---  ?
-//then includes another word or words (characters[A-Za-z0-9_]) ---  \w+
-//matches the previous character 0 or more times --- *
-//matches 0 or more occurences of --- ([.-]?\w+)*
-// --- \w+([.-]?\w+)*
-
-*/
-
+/*regular expression to match a string that includes an alphanumeric phrase followed by:
+a [.] or [-] followed by another sequence of an alphanumeric phrase 1 or more times
+followed by an [@]followed by another word or phrase
+followed by a [.]
+and a word or phrase that is at least 2 letters in length to a maximum of 4 */
 const email_regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
 
 //submit event handler for form validation functions: validName(), validEmail(), activityCheck(), creditCheck()
@@ -183,20 +170,15 @@ $('form').submit(function (event) {
   const validName = () => {
     //if name input text field is blank
     if ($('#name').val() === '') {
-      //prevent form submission
+      //prevent form submission and add error classes/msg
       event.preventDefault();
-      //remove class to prevent duplicate message
       $('.name-error').remove();
-      //add css class
       $('#name').addClass('error');
-      //insert error message into DOM
       $('#name').before('<p class=name-error>Please enter a name.</p>');
-      //add css class
       $('p').addClass('error-text');
-      //scroll to top of page
       $('html,body').scrollTop(0);
+      //otherwise remove error classes allow form submission
     } else {
-      //allow form submission and remove error classes
       $('p').remove('.name-error');
       $('#name').removeClass('error');
     }
@@ -207,57 +189,69 @@ $('form').submit(function (event) {
   const validEmail = (inputText) => {
     //if email input text field does not match regular expression string
     if (!inputText.match(email_regex)) {
-      //prevent form submission
+      //prevent form submission and add error classes/msg
       event.preventDefault();
-      //remove
       $('.email-error').remove();
       $('#mail').addClass('error');
       $('#mail').before('<p class=email-error>Please enter a valid email address.</p>');
       $('p').addClass('error-text');
       $('html,body').scrollTop(0);
     } else {
+      //otherwise remove error classes and allow form submission
       $('p').remove('.email-error');
       $('#mail').removeClass('error');
     }
   };
+  //run email validation
   validEmail($('#mail').val());
 
+  //activity checkbox validation
   const activityCheck = (checked) => {
+    //if no checkbox is checked
     if (checked) {
+      //prevent form submission and add error messages
       event.preventDefault();
       $('.activity-error').remove();
       $('.activities legend').before('<p class=activity-error>Please check at least one activity.</p>');
       $('.activities legend').css('margin-bottom', '1px');
       $('.activity-error').addClass('error-text');
     } else {
+      //otherwise remove error classes and allow form to submit
       $('.activities legend').css('margin-bottom', '22.5px');
       $('p').remove('.activity-error');
     }
   }
+  //run activity isChecked validation
   activityCheck($('input:checked').length<1);
 
+  //payment section validation
   const creditCheck = () => {
     //Break out of creditCheck() validation if credit card option is not selected
     if ($('#payment').val() != 'credit card') {return};
     const cardNumber = $('#cc-num').val();
     const cardNumberValid = (cc) => {
+      //if credit card number field is blank, prevent form submission and add error classes/msg
       if (cc == '') {
         event.preventDefault();
         $('.cc-blank-error, .cc-length-error, .cc-number-error').remove();
         $('.credit-card').before('<p class=cc-blank-error>Please enter a Credit Card Number.</p>')
         $('.cc-blank-error').addClass('error-text');
         $('#cc-num').addClass('error');
+        //if card number text field matches numbers only
       } else if (cardNumber.match(/^\d+$/)) {
+        //if card number length is less than 13 or greater than 16 prevent form submission and add error classes/msg
         if ( cc.length < 13 || cc.length > 16 ) {
           event.preventDefault();
           $('.cc-blank-error, .cc-length-error, .cc-number-error').remove();
           $('.credit-card').before('<p class=cc-length-error>Credit Card Number must be between 13 and 16 digits long.</p>')
           $('.cc-length-error').addClass('error-text');
           $('#cc-num').addClass('error');
+          //if card number length is between 13 and 16 remove errors and allow form to submit
         } else if (cc.length >= 13 && cc.length <= 16) {
           $('.cc-blank-error, .cc-length-error, .cc-number-error').remove();
           $('#cc-num').removeClass('error');
         }
+        //if card number includes more than just numbers prevent submission and add error classes/msg
       } else if (!cardNumber.match(/^\d+$/)) {
         event.preventDefault();
         $('.cc-blank-error, .cc-length-error, .cc-number-error').remove();
@@ -266,28 +260,39 @@ $('form').submit(function (event) {
         $('#cc-num').addClass('error');
       }
     }
+    //run card number validation
     cardNumberValid($('#cc-num').val());
 
+    //zip code validation
     const cardZipValid = () => {
       const zipCode = $('#zip').val();
+      //if zip code field is blank
       if (zipCode == '') {
+        //prevent form submission and add error classes/msg
         event.preventDefault();
         $('.zip-blank-error, .zip-length-error, .zip-number-error').remove();
         $('.credit-card').before('<p class=zip-blank-error>Please enter a Zip Code.</p>');
         $('.zip-blank-error').addClass('error-text');
         $('#zip').addClass('error');
+        //if zip code field input matches a number
       } else if (zipCode.match(/^\d+$/)) {
+        //if the zip code number input is not equal to 5
         if (zipCode.length != 5) {
+          //prevent form submission and add error classes/msg
           event.preventDefault();
           $('.zip-blank-error, .zip-length-error, .zip-number-error').remove();
           $('.credit-card').before('<p class=zip-length-error>Zip Code must be 5 digits long.</p>');
           $('.zip-length-error').addClass('error-text');
           $('#zip').addClass('error')
+          //if the zip code number input is equal to 5
         } else if (zipCode.length == 5) {
+          //remove errors and allow form to submit
           $('.zip-blank-error, .zip-length-error, .zip-number-error').remove();
           $('#zip').removeClass('error');
         }
+        //if the zip code field input includes characters other than numbers
       } else if (!zipCode.match(/^\d+$/)){
+        //prevent form submission and add error classes/msg
         event.preventDefault();
         $('.zip-blank-error, .zip-length-error, .zip-number-error').remove();
         $('.credit-card').before('<p class=zip-number-error>Zip Code cannot include letters or symbols.')
@@ -295,28 +300,39 @@ $('form').submit(function (event) {
         $('#zip').addClass('error');
       }
     }
+    //run zip code field validation
     cardZipValid();
 
+    //CVV field validation
     const cardCVV = () => {
       const CVV = $('#cvv').val();
+      //if cvv field is blank
       if (CVV == '') {
+        //prevent form submission and add error classes
         event.preventDefault();
         $('.cvv-blank-error, .cvv-length-error, .cvv-number-error').remove();
         $('.credit-card').before('<p class=cvv-blank-error>Please enter a CVV.</p>');
         $('.cvv-blank-error').addClass('error-text');
         $('#cvv').addClass('error');
+        //if the cvv field matches a string of only numbers
       } else if (CVV.match(/^\d+$/)) {
+        //if the cvv field string length is not equal to 3
         if (CVV.length != 3) {
+          //prevent form submission and add error classes
           event.preventDefault();
           $('.cvv-blank-error, .cvv-length-error, .cvv-number-error').remove();
           $('.credit-card').before('<p class=cvv-length-error>CVV must be 3 digits long.</p>');
           $('.cvv-length-error').addClass('error-text');
           $('#cvv').addClass('error');
+          //if cvv field string length is equal to 3
         } else if (CVV.length == 3) {
+          //remove errors and allow form submission
           $('.cvv-blank-error, .cvv-length-error, .cvv-number-error').remove();
           $('#cvv').removeClass('error');
         }
+        //if the cvv field string does not match only numbers
       } else if (!CVV.match(/^\d+$/)){
+        //prevent form submission and add error classes/msg
         event.preventDefault();
         $('.cvv-blank-error, .cvv-length-error, .cvv-number-error').remove();
         $('.credit-card').before('<p class=cvv-number-error>CVV cannot include letters or symbols.')
@@ -324,8 +340,10 @@ $('form').submit(function (event) {
         $('#cvv').addClass('error');
       }
     }
+    //run cvv validation
   cardCVV();
   }
+  //run payment section validation
   creditCheck();
 });
 
@@ -333,20 +351,27 @@ $('form').submit(function (event) {
 //REAL-TIME EMAIL VALIDATION
 /////////////////////////////
 
+
+//keyup event handler to run each time a key is pressed and released
 $('#mail').first().keyup(function() {
     var $email = this.value;
     validateEmail($email);
 });
 
 function validateEmail(inputText) {
+    //email regular expression string to match email input text field against
     var email_regex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    //if current string does not match email_regex string template
     if (!email_regex.test(inputText)) {
+      //display error classes/msg
       $('.email-error, .email-valid').remove();
       $('#mail').addClass('error');
       $('#mail').before('<p class=email-error>Please enter a valid email address (eg: "John.Doe@hotmail.com")</p>');
+      //if input field is currently empty, remove error classes
     } else if ($('#mail').val()=='') {
       $('.email-valid, .email-error').remove();
       $('#mail').removeClass('error');
+      //if user email string matches regular expression display validation pass message
     } else {
       $('p').remove('.email-error');
       $('#mail').removeClass('error');
